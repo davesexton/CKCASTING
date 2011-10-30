e311ce2d43baf53894b426c6491d124d337cde26
Dave Sexton <davesexton1@googlemail.com>
2011-10-30 15:30:40 +0000

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ckcasting::Application.config.secret_token = '7137d90c2f682b4e73a03ae307b534a79df457eded75c1f6037682b02c42421514007e8855293b0807594e4f31d39d868a3911bccf47164f3f703d716bf8468d'
