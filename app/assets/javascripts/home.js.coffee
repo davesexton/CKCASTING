cb3b96094038e3e56b618a2b5dc9689d863aba70
Dave Sexton <davesexton1@googlemail.com>
2012-07-09 18:30:36 +0100

    clean out files no longer needed

--- app/assets/javascripts/home.js.coffee
+++ app/assets/javascripts/home.js.coffee
@@ -6,7 +6,7 @@ imgs = []
 $ ->
   $('nav > div').click( (e) ->
     window.location.href =  $(@).children(0).get(0).href
-  ).hover( (e) -> 
+  ).hover( (e) ->
     $(@).css('cursor','pointer')
   )
 
@@ -16,7 +16,7 @@ $ ->
 #Global variables
 #----------------------------------------
       canvas = $("#carousel").get(0)
-      context = canvas.getContext "2d" 
+      context = canvas.getContext "2d"
       canvasWidth = canvas.width
       canvasHeight = canvas.height
       radiusX = 375
@@ -28,30 +28,18 @@ $ ->
       centerX = (canvasWidth / 2) - 46
       centerY = (canvasHeight / 2) - 77
       speed = 0.015
-      
+
 #Elipse function
 #----------------------------------------
       drawEllipse = (c, x, y, w, h) ->
         c.beginPath()
-        c.moveTo x + w, y 
+        c.moveTo x + w, y
         c.bezierCurveTo x + w, y + h, x - w, y + h, x - w, y
         c.bezierCurveTo x - w, y - h, x + w, y - h, x + w, y
         c.fill()
         c.closePath()
-        
-#WetFloor function
-#----------------------------------------
-#      wetFloor = (imgObj, w, h) ->
-#        can = $('<canvas width="' + w + '" height="' + h + '"></canvas>').get(0)
-#        can.width = w
-#        can.height = h
-#        ctx = can.getContext "2d"
-#        ctx.drawImage imgObj, 0, 0, w, h
-#        imgData = ctx.getImageData(0, 0, 156, 30)
-#        data = imgData.data
-#        data
 
-#Hover handler      
+#Hover handler
 #----------------------------------------
       $(canvas).mousemove (e)->
         mouseX = e.pageX - @offsetLeft
@@ -59,9 +47,9 @@ $ ->
         speed = ((canvasWidth / 2) - mouseX) / 10000
         currentId = null
         for img in imgs
-          if mouseX > img.x() and 
-          mouseX < img.x() + img.width() and 
-          mouseY > img.y() and 
+          if mouseX > img.x() and
+          mouseX < img.x() + img.width() and
+          mouseY > img.y() and
           mouseY < img.y() + (img.height() * 0.75)
             currentId = img.id
             $(@).css('cursor','pointer')
@@ -69,15 +57,15 @@ $ ->
         if currentId == null
           $(@).css('cursor','auto')
           showClick = false
-        
 
-#Click handler              
-#----------------------------------------      
+
+#Click handler
+#----------------------------------------
       $(canvas).click (e)->
         showClick = false
         window.location.href = currentId
 
-#Define image object      
+#Define image object
 #----------------------------------------
       class imgObj
         constructor: (@img, @angle, @id) ->
@@ -117,15 +105,15 @@ $ ->
           if @img.complete
             c.globalCompositeOperation = 'source-atop'
             c.fillRect @x() - 10, @ny() + 10, @width(), @nheight() - 10
-            c.fillRect @x() + 10, @py() + 10, @width(), @pheight() - 10 
+            c.fillRect @x() + 10, @py() + 10, @width(), @pheight() - 10
             c.globalCompositeOperation = 'source-over'
           c.drawImage @img, @x(), @y() , @width(), @height()
           @angle += s
           @nangle = @angle + s
           @pangle = @angle - s
- 
+
 #initalise image objects
-#----------------------------------------          
+#----------------------------------------
       imgSet = $('#carouselImages img')
       totImgs = imgSet.length
       imgSet.each((i, e) ->
@@ -133,28 +121,28 @@ $ ->
         id = '/castbook/cast/' + regex.exec(e.src)[1]
         angle = i * ((Math.PI * 2) / totImgs)
         img = new imgObj(e, angle, id)
-        
+
         imgs.push img
       )
-      
+
 #animate function
 #----------------------------------------
       context.font="12px Verdana"
       animate = ->
-        context.clearRect 0, 0, canvasWidth, canvasHeight 
-        
+        context.clearRect 0, 0, canvasWidth, canvasHeight
+
         context.fillStyle = "rgba(0, 0, 0, 0.2)"
         imgs.sort((a, b)-> a - b)
         for img, i in imgs
           img.draw context, speed
-  
+
           if i == Math.round(totImgs / 2)
             #context.fillStyle = "rgba(0, 0, 0, 1)"
             context.globalCompositeOperation = 'destination-over'
             #context.fillText 'click here', mouseX, moseY
             drawEllipse context, centerX, centerY + 110, radiusX, radiusY + 10
             #context.globalCompositeOperation = 'source-over'
-            #context.fillStyle = "rgba(0, 0, 0, 0.2)"  
+            #context.fillStyle = "rgba(0, 0, 0, 0.2)"
         if showClick
           context.save()
           context.fillStyle = "rgba(255, 255, 255, 0.8)"
--- script/migrate_live_site.rb
+++ script/migrate_live_site.rb
@@ -1,113 +1,116 @@
 #!/usr/bin/env ruby
 
+require 'hpricot'
+require 'open-uri'
 require "net/http"
 
+# ----- Delete images
 Dir.chdir('/home/dave/websites/ckcasting/app/assets/images/cast_images')
 Dir['*.jpg'].each { |f| File.delete f }
 
-new_id = 0
-image_count = 0
-x = ''
-seed = '# encoding: utf-8\n'
-Net::HTTP.start("www.ckcasting.co.uk") do |http|
-	('1'..'700').each do |f| 
-		
-		resp = http.get("/castbook/#{f}.jpg")
-		if resp.code == '200'
-			body = http.get("/castbook.aspx?id=#{f}").body
-			
-			if body =~ /Very sorry but/
-				puts "Found file #{f}.jpg but no cast details"
-				
-			else
-				# Create seed
-				seed += 'Person.create('
+# ----- Set variables
+new_id = 1
+seed = "# encoding: utf-8\n"
 
-				# Get name
-				person_name = body.match(/FormViewCastDetails_NAMELabel"\>(.+)\<\/span/)[1]				
-				puts "Found #{f}.jpg '#{person_name}' remapping as CAST ID #{new_id += 1}"
-				person_last_name = person_name.match(/\w*$/)[0]
-				person_first_name = person_name.chomp(person_last_name).strip
-				seed += ":first_name => '#{person_first_name}', "
-				seed += ":last_name => '#{person_last_name}', "
+('46'..'600').each do |f|
+  doc = Hpricot(open("http://www.ckcasting.co.uk/castbook.aspx?id=#{f}"))
 
-				# Get height
-				height_in = body.match(/FormViewCastDetails_HEIGHT_INLabel"\>(.*)\<\/span/)[1]
-				height_ft = body.match(/FormViewCastDetails_HEIGHT_FTLabel"\>(.*)\<\/span/)[1]
-				seed += ":height_feet => #{height_ft}, :height_inches => #{height_in}, "
+# ----- Get name
+  name = (doc/'//*[@id="ctl00_MainContentPlaceHolder_FormViewCastDetails_NAMELabel"]').innerHTML
+  next if name == ''
+  puts "Found '#{name}' - remapping CAST ID #{f} => #{new_id}"
+  last_name = name.match(/\w*$/)[0]
+  first_name = name.chomp(last_name).strip
 
-				# Get hair and eye colour
-				hair_colour = body.match(/FormViewCastDetails_HAIR_COLOURLabel"\>(.*)\<\/span/)[1]
-				eye_colour = body.match(/FormViewCastDetails_EYE_COLOURLabel"\>(.*)\<\/span/)[1]
-				seed += ":hair_colour => '#{hair_colour}', :eye_colour => '#{eye_colour}', "
+# ----- Get date of birth
+  date_of_birth = (doc/'//*[@id="ctl00_MainContentPlaceHolder_FormViewCastDetails_HiddenFieldDOB"]').attr(:value)
 
-				# Get gender
-				gender = body.match(/FormViewCastDetails_SEXLabel"\>(.*)\<\/span/)[1]
-				seed += ":gender => '#{gender}', "
 
-				# Get view data
-				last_viewed_at = body.match(/FormViewCastDetails_LAST_VIEWEDLabel"\>(.*)\<\/span/)[1]
-				view_count = body.match(/FormViewCastDetails_VIEW_COUNTLabel"\>(.*)\<\/span/)[1]
-				seed += ":last_viewed_at => '#{last_viewed_at}', :view_count => #{view_count}, "
+# ----- Get height
+  feet = (doc/'//*[@id="ctl00_MainContentPlaceHolder_FormViewCastDetails_HEIGHT_FTLabel"]').innerHTML
+  inches = (doc/'//*[@id="ctl00_MainContentPlaceHolder_FormViewCastDetails_HEIGHT_INLabel"]').innerHTML
 
-				seed += ":status => 'Active' ) \n"
-							
-				body.gsub!("\r\n"," ")
+# ----- Get colors
+  hair = (doc/'//*[@id="ctl00_MainContentPlaceHolder_FormViewCastDetails_HAIR_COLOURLabel"]').innerHTML
+  eye = (doc/'//*[@id="ctl00_MainContentPlaceHolder_FormViewCastDetails_EYE_COLOURLabel"]').innerHTML
 
-				body.match(/_InterestsBulletedList"\>(.+?)\/ul/) do |is| 
-					c = 1
-					unless is == nil
-						(is[1].scan(/li\>(.+?)\<\/li\>/)).each do |i| 
-							txt = i[0].gsub(/[']/, '\\\\\'').strip
-							txt.gsub!(/ +/, ' ')
-							txt.gsub!(/&amp;/, 'and')
-							seed += "Skill.create(:Person_id => #{new_id}, :display_order => #{c}, :skill_text => '#{txt}')\n"
-							c += 1
-						end
-					end
-				end
+# ----- Get sex
+  sex = (doc/'//*[@id="ctl00_MainContentPlaceHolder_FormViewCastDetails_SEXLabel"]').innerHTML
 
-				body.match(/_CreditsBulletedList"\>(.+?)\/ul/) do |is| 
-					c = 1
-					unless is == nil
-						(is[1].scan(/li\>(.+?)\<\/li\>/)).each do |i| 
-							txt = i[0].gsub(/[']/, '\\\\\'').strip
-							txt.gsub!(/ +/, ' ')
-							txt.gsub!(/&amp;/, 'and')
-							txt.gsub!(/Richard 11/, 'Richard II')
-							txt.gsub!(/ _ /, ' - ')
-							txt.gsub!(/Paul O Grady/, 'Paul O\\\'Grady')
-							
-							if txt.match(/1939.+Poliakoff Feature Film/)
-								txt = '\\\'1939\\\' Poliakoff Feature Film'
-							end
-							if txt.match(/Oliver.+Theatre Royal/)
-								txt = 'Oliver - Theatre Royal'
-							end
-							if txt.match(/Nine.+Feature Film/)
-								txt = 'Nine - Feature Film'
-							end
+# ----- Get view counts
+  last_viewed = (doc/'//*[@id="ctl00_MainContentPlaceHolder_FormViewCastDetails_LAST_VIEWEDLabel"]').innerHTML
+  view_count = (doc/'//*[@id="ctl00_MainContentPlaceHolder_FormViewCastDetails_VIEW_COUNTLabel"]').innerHTML
 
-							seed += "Credit.create(:Person_id => #{new_id}, :display_order => #{c}, :credit_text => '#{txt}')\n"
-							c += 1
-						end
-					end
-				end
-				
-				seed.gsub!('=> ,', '=> nil,')
-				#x = body
-				open("#{new_id}.jpg", "wb") { |file| file.write(resp.body) }
-				
-			end
-			image_count += 1
-		end
-	end
+# ----- Add to seed file
+  seed += "Person.create(:first_name => '#{first_name}'" \
+   + ", :last_name => '#{last_name}'" \
+   + ", :date_of_birth => '#{date_of_birth}'" \
+   + ", :height_feet => #{feet}" \
+   + ", :height_inches => #{inches}" \
+   + ", :hair_colour => '#{hair}'" \
+   + ", :eye_colour => '#{eye}'" \
+   + ", :gender => '#{sex}'" \
+   + ", :last_viewed_at => '#{last_viewed}'" \
+   + ", :view_count => #{view_count}" \
+   + ", :status => 'Active' )\n"
+
+# ----- Get skills
+  ord = 1
+  txt = ''
+  (doc/'//*[@id="ctl00_MainContentPlaceHolder_InterestsBulletedList"]/li').each do |e|
+    txt = e.innerHTML.strip
+
+    seed += "Skill.create(:Person_id => #{new_id}" \
+     + ", :display_order => #{ord}" \
+     + ", :skill_text => '#{txt}')\n"
+    ord += 1
+  end
+
+# ----- Get credits
+  ord = 1
+  txt = ''
+  (doc/'//*[@id="ctl00_MainContentPlaceHolder_CreditsBulletedList"]/li').each do |e|
+    txt = e.innerHTML.strip
+    txt.gsub!('&amp;', 'and')
+    txt.gsub!(' The ', ' the ')
+    txt.gsub!(' Technicolor ', ' Technicolour ')
+    txt.gsub!(/ +/, ' ')
+    txt.gsub!(/\.$/, '')
+    txt.gsub!(/[']/, '\\\\\'')
+    txt.gsub!(/ _ /, ' - ')
+    txt.gsub!('Good Night', 'Goodnight')
+    txt.gsub!('Kings Head', 'King\\\'s Head')
+    txt.gsub!('Drama -TV', 'Drama - TV')
+    txt.gsub!(/Sex.+Drugs.+Rock.+Roll/, 'Sex & Drugs & Rock & Roll')
+    txt.gsub!('Toonatik TV', 'Toonattik')
+    txt.gsub!('Disney Land', 'Disneyland')
+    txt.gsub!('W.E ', 'W.E. ')
+    txt.gsub!('Kenneth More ', 'Kenneth Moore ')
+    txt.gsub!('CBBC- ', 'CBBC - ')
+
+    txt.gsub!(/Paul O.*Grady/, 'Paul O\\\'Grady')
+
+    seed += "Credit.create(:Person_id => #{new_id}" \
+     + ", :display_order => #{ord}" \
+     + ", :credit_text => '#{txt}')\n"
+    ord += 1
+  end
+
+# ----- Get image file
+  Net::HTTP.start("www.ckcasting.co.uk") do |http|
+    resp = http.get("/castbook/#{f}.jpg")
+    open("#{new_id}.jpg", "wb") { |file| file.write(resp.body) } if resp.code == '200'
+
+  end
+
+# ----- Next ID
+  new_id += 1
 end
-puts " "
-puts "Found #{image_count} JPGs and #{new_id} cast members"
 
+# ----- Finished
+seed.gsub!('=> ,', '=> nil,')
+seed.gsub!("=> '',", '=> nil,')
 Dir.chdir('/home/dave/websites/ckcasting/db')
 File.open('seeds.rb', "w") { |file| file.write(seed) }
-#File.open('temp.txt', "w") { |file| file.write(x) }
-#puts x
-# FormViewCastDetails_NAMELabel">Delilah Gendler</span>
+puts " "
+puts "Found #{new_id - 1} cast members"
