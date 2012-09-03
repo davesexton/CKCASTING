f48deff3e26a5d60714145b14e8a06023758a2b0
Dave Sexton <davesexton1@googlemail.com>
2012-09-03 22:45:24 +0100

    Working on cast edit page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,5 @@ Ckcasting::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
 end
