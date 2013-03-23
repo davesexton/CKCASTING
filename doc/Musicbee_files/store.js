f48e9800f1a32565581f5dd59ff3a38e764f2dc0
Dave Sexton <davesexton1@googlemail.com>
2013-03-23 18:49:19 +0000

    fixed year when edting a person

---
+++ doc/Musicbee_files/store.js
@@ -0,0 +1,18 @@
+(function() {
+  var d;
+
+  d = document;
+
+  d.createElement('header');
+
+  d.createElement('nav');
+
+  d.createElement('hgroup');
+
+  d.createElement('footer');
+
+  d.createElement('section');
+
+  d.createElement('aside');
+
+}).call(this);
